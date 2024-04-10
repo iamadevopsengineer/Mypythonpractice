843d7ede654ecb80f9c2bade8bb0d2706968331a
iamadevopsengineer <iamadevopsengineer2024@gmail.com>
2024-04-09 22:34:17 -0400

    First commit

---
+++ .history/class2/class_2_variables_operators_20240409222616.py
@@ -0,0 +1,91 @@
+# Addition
+
+
+# Subtraction
+
+
+# Multiplication
+
+
+# Division
+
+
+# Exponents
+
+
+# Integer Division
+
+
+# Modulo/Mod/Remainder
+
+
+# Program to find the perimeter of a rectangle
+
+
+# Data Types
+
+
+# Integer
+
+
+# String
+
+
+# Bool
+
+
+# Float
+
+
+# String
+
+
+# List 
+
+
+# Dictionary
+
+
+# Cast a string to an integer
+
+
+
+# Cast integer to string
+
+
+# Student grade dataset
+
+
+# finding amount of assignments
+
+
+# Perimeter of a rectangle
+
+
+# Determine perimeter and display output
+
+
+# Fahrenheit to Celsius
+
+
+# Eval
+
+
+'''
+# You are given a triangle with a side #1 of 4, base of 6, and side #2 of 3. Create
+a brief python script that will determine the perimeter of the triangle. Comment your code
+
+1. Print the perimeter
+2. Using boolean operators is side #1 greater than the base?
+3. Using boolean operators is side #2 less than the base?
+4. Using boolean operators is base larger than or equal to side #1?
+'''
+
+
+
+
+
+
+
+
+
